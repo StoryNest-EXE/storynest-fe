0b401d4655dee28998a6289c57afa882bf0d7683
SapphrieMoon <caidenlam@gmail.com>
2025-08-26 11:31:00 +0700

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true, // bật StrictMode để cảnh báo bug tiềm ẩn (khuyên dùng)
+  swcMinify: true        // dùng SWC để minify (mặc định bật từ Next 13 trở đi)
+}
+
+export default nextConfig
