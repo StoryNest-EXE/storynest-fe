7ad36875cba7187962e1b437b6b312d609b93244
SapphrieMoon <caidenlam@gmail.com>
2025-09-13 16:52:17 +0700

    fix: fix deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,11 @@ import Image from "next/image";
 
 export default function Home() {
   const toekPayload = useAuth();
-  const handleRefresh = () => {
-    useRefreshToken();
-    console.log("refresh nè");
+  const refresh = useRefreshToken(); // ✅ Hook phải khai báo ở top-level
+
+  const handleRefresh = async () => {
+    const newToken = await refresh();
+    console.log("Refresh result:", newToken);
   };
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
